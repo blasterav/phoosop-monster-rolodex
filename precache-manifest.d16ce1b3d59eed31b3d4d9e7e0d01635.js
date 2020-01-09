e9d030d441d25ead0762b01afc2c68e0b76fb73a
Aleksei Rygzynov <aleksei.ryg@ascendcorp.com>
2020-01-09 14:22:24 +0700

    Updates

---
+++ precache-manifest.d16ce1b3d59eed31b3d4d9e7e0d01635.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "744e84719e87cdafe262da949f2c06e1",
+    "url": "/phoosop-monster-rolodex/index.html"
+  },
+  {
+    "revision": "26e627840b37a076092d",
+    "url": "/phoosop-monster-rolodex/static/css/main.35fea07d.chunk.css"
+  },
+  {
+    "revision": "c728ba4a2366e885ec54",
+    "url": "/phoosop-monster-rolodex/static/js/2.d8d20882.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/phoosop-monster-rolodex/static/js/2.d8d20882.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26e627840b37a076092d",
+    "url": "/phoosop-monster-rolodex/static/js/main.59f9ea40.chunk.js"
+  },
+  {
+    "revision": "b95ccff5280f1ab9b518",
+    "url": "/phoosop-monster-rolodex/static/js/runtime-main.bf3b7ac9.js"
+  }
+]);
